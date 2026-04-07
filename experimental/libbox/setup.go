ab630a748220c4d9047b7b5f759f7cafe8f31f3d
世界 <i@sekai.icu>
2026-04-07 20:52:06 +0800

    platform: Fix set local

--- experimental/libbox/setup.go
+++ experimental/libbox/setup.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"runtime"
 	"runtime/debug"
+	"strings"
 	"time"
 
 	"github.com/sagernet/sing-box/common/networkquality"
@@ -16,6 +17,7 @@ import (
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/service/oomkiller"
 	"github.com/sagernet/sing/common/byteformats"
+	E "github.com/sagernet/sing/common/exceptions"
 )
 
 var (
@@ -100,8 +102,14 @@ func Setup(options *SetupOptions) error {
 	return redirectStderr(filepath.Join(sWorkingPath, "CrashReport-"+sCrashReportSource+".log"))
 }
 
-func SetLocale(localeId string) {
-	locale.Set(localeId)
+func SetLocale(localeId string) error {
+	if strings.Contains(localeId, "@") {
+		localeId = strings.Split(localeId, "@")[0]
+	}
+	if !locale.Set(localeId) {
+		return E.New("unsupported locale: ", localeId)
+	}
+	return nil
 }
 
 func Version() string {
